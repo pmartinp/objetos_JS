1faa859fe244445c10a61718aef5772f55f1a541
pmartinp <pmartinpdev@gmail.com>
2022-11-15 10:06:26 +0100

    ejercicio 34

--- 034/034findLastDayOfMonth.js
+++ 034/034findLastDayOfMonth.js
@@ -6,7 +6,9 @@ month – el mes, de 0 a 11.
 Por ejemplo, getLastDayOfMonth(2012, 1) = 29 (febrero, año bisiesto).
 */
 
+alert(getLastDayOfMonth(2012, 1));
 
-function getLastDayOfMonth(year, month){
-  
+function getLastDayOfMonth(year, month) {
+  let getLastDayOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  return (year % 4 == 0 && month == 1 ) ? getLastDayOfMonth[month]+1 : getLastDayOfMonth[month]
 }
