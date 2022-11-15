b932073f8c30803b5fc2dcc43fc1f1fa107bf324
pmartinp <pmartinpdev@gmail.com>
2022-11-15 18:36:05 +0100

    cambio nombre de variable ej34

--- 034/034findLastDayOfMonth.js
+++ 034/034findLastDayOfMonth.js
@@ -9,7 +9,7 @@ Por ejemplo, getLastDayOfMonth(2012, 1) = 29 (febrero, año bisiesto).
 alert(getLastDayOfMonth(2012, 1));
 
 function getLastDayOfMonth(year, month) {
-  let getLastDayOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let LastDayOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   // Si el año es bisiesto y cae en febrero devolvemos el 28+1, de otro modo el último día normal
-  return (year % 4 == 0 && month == 1 ) ? getLastDayOfMonth[month]+1 : getLastDayOfMonth[month]
+  return (year % 4 == 0 && month == 1 ) ? LastDayOfMonth[month]+1 : LastDayOfMonth[month]
 }
